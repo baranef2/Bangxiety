5d4293f5d3e2071221d674c665640a3b47bf8c50
baranef2 <befeoglu2@gmail.com>
2025-09-06 19:56:30 +0300

    Player ve Get_Ammo

--- Assets/Scripts/Functions/PlayerFunc/Get_Ammo.cs
+++ Assets/Scripts/Functions/PlayerFunc/Get_Ammo.cs
@@ -28,17 +28,10 @@ public class Get_Ammo : MonoBehaviour
     }
 }
     
-}
     
     // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
+   
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
